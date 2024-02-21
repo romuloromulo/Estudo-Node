144d456577620849735d42268f0b2fc99e313ad5
romuloromulo <neoromulo@gmail.com>
2024-02-21 14:25:18 -0300

    estudo

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head> <title>foda-se</title> </head>");
+  }
+});
+
+server.listen();
