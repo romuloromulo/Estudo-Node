2872e253267ddb9fed9745a836d78a5c28df7c1c
romuloromulo <neoromulo@gmail.com>
2024-02-21 15:45:51 -0300

    estudando req

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head> <title>foda-se</title> </head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Enviar</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
 });
 
-server.listen();
+server.listen(3000);
