d5fce93bd8999710077984e05f46b2253476a10d
romuloromulo <neoromulo@gmail.com>
2024-02-23 21:29:32 -0300

    aprendendo fundamentos do express

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = require("http");
-const requestHandler = require("./route");
+const express = require("express");
+const app = express();
 
-const server = http.createServer(n);
+app.use((req, resp, next) => {
+  console.log("Teste!2");
+  next();
+});
+app.use((req, resp, next) => {
+  console.log("Teste!");
+  next();
+});
 
-server.listen(3000);
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Servidor está ouvindo na porta 3000");
+});
--- exercicio/app.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const requestHandler = require("./route");
-
-const server = http.createServer(requestHandler);
-
-server.listen(3000);
