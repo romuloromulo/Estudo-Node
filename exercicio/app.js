08da560a7e2e165abf59226ca03760c9b9ba425b
romuloromulo <neoromulo@gmail.com>
2024-02-26 14:28:34 -0300

    adicionadno exercicio

---
+++ exercicio/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>pagina user</h1>");
+});
+app.use("/", (req, res, next) => {
+  res.send("<h1>pagina 1</h1>");
+});
+
+app.listen(3000);
