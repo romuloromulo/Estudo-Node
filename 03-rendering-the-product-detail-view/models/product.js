ae1eea54ff4d5653769bccb70d7482ac235bca8a
romuloromulo <neoromulo@gmail.com>
2024-03-06 22:46:29 -0300

    como usar dados dinamicos no node

--- 03-rendering-the-product-detail-view/models/product.js
+++ 03-rendering-the-product-detail-view/models/product.js
@@ -1,18 +1,17 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
     } else {
-      console.log("PARCONTENT", JSON.parse(fileContent));
       cb(JSON.parse(fileContent));
     }
   });
@@ -28,10 +27,9 @@ module.exports = class Product {
 
   save() {
     this.id = Math.random().toString();
-
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
@@ -42,8 +40,8 @@ module.exports = class Product {
   }
 
   static findById(id, cb) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id);
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
       cb(product);
     });
   }
