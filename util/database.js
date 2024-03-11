4ab336da2b6ec60be33b3a3085ad3139e1eef78b
romuloromulo <neoromulo@gmail.com>
2024-03-11 17:29:37 -0300

    refatorando métodos para lidar co m o db

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 require("dotenv").config();
-const senha = process.env.DATABASE_PASSWORD;
+
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "curso-node-schema",
-  password: senha,
+  password: process.env.DATABASE_PASSWORD,
 });
 
 module.exports = pool.promise();
