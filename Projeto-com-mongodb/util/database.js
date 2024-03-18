a75b042e1da9372ba3c29301c799c9612dadc5cf
romuloromulo <neoromulo@gmail.com>
2024-03-18 16:58:03 -0300

    estudando mongodb com node

--- Projeto-com-mongodb/util/database.js
+++ Projeto-com-mongodb/util/database.js
@@ -1,13 +1,20 @@
-const { Sequelize } = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize(
-  "curso-node-schema",
-  "root",
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: "mysql",
-    host: "localhost",
+async function connectToMongo(cb) {
+  try {
+    const client = await MongoClient.connect(
+      "mongodb+srv://romulovianadev:1pQL5n0ocJQjSRDP@estudonode.sthnf7e.mongodb.net/?retryWrites=true&w=majority&appName=estudonode",
+      { useNewUrlParser: true, useUnifiedTopology: true } // adicionando as opções de conexão
+    );
+    console.log("Conexão estabelecida com sucesso!");
+    cb(client);
+    // Faça o que for necessário com o cliente MongoDB aqui
+    // Por exemplo: client.db("nomeDoBancoDeDados").collection("nomeDaColecao").find()...
+  } catch (error) {
+    console.error("Erro ao conectar ao MongoDB:", error);
   }
-);
+}
 
-module.exports = sequelize;
+// Chamando a função para conectar ao MongoDB
+module.exports = connectToMongo;
