41309fe7fdad375968804679ae464472b5edc656
romuloromulo <neoromulo@gmail.com>
2024-03-19 16:09:15 -0300

    adicionando model user

--- Projeto-com-mongodb/models/user.js
+++ Projeto-com-mongodb/models/user.js
@@ -1,16 +1,35 @@
-const { Sequelize } = require("sequelize");
+const { getDb } = require("../util/database");
+const mongodb = require("mongodb");
+const db = getDb();
 
-const sequelize = require("../util/database");
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+  async save() {
+    try {
+      await db.collection("users").insertOne(this);
+      console.log("Novo user criado com sucesso");
+    } catch (error) {
+      console.error("Erro ao salvar user:", error);
+      throw error;
+    }
+  }
+
+  static async findById(prodId) {
+    try {
+      const user = await db
+        .collection("users")
+        .findOne({ _id: new mongodb.ObjectId(prodId) });
+      console.log("chamando", user);
+      return user;
+    } catch (error) {
+      console.error("Erro ao buscar user por ID:", error);
+      throw error;
+    }
+  }
+}
 
 module.exports = User;
